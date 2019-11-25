c3c0228317b5b5c991d36682c508d24cacc9e767
Umberto Palazzo <upalazzo93@gmail.com>
2019-11-25 22:01:03 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_215858) do
-
-  create_table "dogs", force: :cascade do |t|
-    t.string "name"
-    t.string "breed"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
